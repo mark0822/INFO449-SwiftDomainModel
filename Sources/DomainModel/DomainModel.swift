756c9d0661e6a1556ac05e6298503081067a17e2
mark0822 <mark20020822@gmail.com>
2024-04-10 04:21:46 -0700

    completed money

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -9,6 +9,39 @@ struct DomainModel {
 // Money
 //
 public struct Money {
+    var amount : Int
+    var currency : String
+    
+    func convert (_ c: String ) -> Money{
+        var usdAmount: Int
+        var convertedAmount : Int
+        switch self.currency{
+          case "GBP":
+            usdAmount = self.amount * 2
+          case "EUR":
+            usdAmount = self.amount * 2 / 3
+          case "CAN":
+            usdAmount = self.amount * 4 / 5
+          default:
+            usdAmount = self.amount * 1
+        }
+        switch c{
+          case "GBP":
+            convertedAmount = usdAmount / 2
+          case "EUR":
+            convertedAmount = usdAmount * 3 / 2
+          case "CAN":
+            convertedAmount = usdAmount * 5 / 4
+          default:
+            convertedAmount = usdAmount * 1
+        }
+        return Money(amount: convertedAmount, currency: c)
+    }
+    
+    func add (_ money: Money) -> Money {
+        let convertedMoney = self.convert(money.currency)
+        return Money(amount: convertedMoney.amount + money.amount, currency: money.currency)
+    }
 }
 
 ////////////////////////////////////
