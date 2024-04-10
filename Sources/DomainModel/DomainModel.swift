bbf3252414142a4f8a3a42d64302425f322377be
mark0822 <mark20020822@gmail.com>
2024-04-10 06:02:22 -0700

    finished, passed all tests

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -85,18 +85,88 @@ public class Job {
         }
         return
     }
-    
-    
 }
 
 ////////////////////////////////////
 // Person
 //
 public class Person {
+    var firstName : String
+    var lastName : String
+    var age : Int
+    private var _job : Job? = nil
+    var job : Job? {
+        get {
+            return _job
+        }
+        set {
+            if self.age >= 16 {
+                _job = newValue
+            } else {
+                print("Too young to work")
+            }
+        }
+    }
+    var _spouse : Person? = nil
+    var spouse : Person? {
+        get {
+            return _spouse
+        }
+        set {
+            if self.age >= 18 {
+                _spouse = newValue
+            } else {
+                print("Too young to get married")
+            }
+        }
+    }
+    
+    init(firstName: String, lastName: String, age: Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+    }
+    
+    func toString () -> String {
+        return "[Person: firstName:\(self.firstName) lastName:\(self.lastName) age:\(self.age) job:\(String(describing: self.job?.type)) spouse:\(String(describing: self.spouse?.firstName))]"
+    }
 }
 
 ////////////////////////////////////
 // Family
 //
 public class Family {
+    var members : [Person] = []
+    
+    init(spouse1 : Person, spouse2: Person) {
+        if spouse1.spouse == nil && spouse2.spouse == nil {
+            spouse1.spouse = spouse2
+            spouse2.spouse = spouse1
+            self.members.append(spouse1)
+            self.members.append(spouse2)
+        }
+    }
+    
+    func haveChild (_ child : Person) -> Bool {
+        if members[0].age < 21 && members[1].age < 21 {
+            return false
+        }
+        self.members.append(child)
+        return true
+    }
+    
+    func householdIncome () -> Int {
+        var totalIncome : Int = 0
+        for person in members {
+            if person.job != nil {
+                switch person.job!.type{
+                  case .Salary(let salaryAmount):
+                    totalIncome += Int(salaryAmount)
+                  case .Hourly(let hourlyAmount):
+                    totalIncome += Int(hourlyAmount * 2000.0)
+                }
+            }
+        }
+        return totalIncome
+    }
 }
